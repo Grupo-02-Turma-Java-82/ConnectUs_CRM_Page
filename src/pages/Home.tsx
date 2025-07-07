d98d39d4e19ab0f746dfce0bb072f74040e584ad
Luiza_Gonck <luizae.goncalves@hotmail.com>
2025-07-07 08:51:06 -0300

    :tada: Commit Inicial - Implementando a  Navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,18 @@
 import { ChooseUs } from "@/components/ChooseUs";
 import { Features } from "@/components/Features";
-import { Footer } from "@/components/Footer";
 import { GoDashboard } from "@/components/GoDashboard";
 import { Hero } from "@/components/Hero";
-import { Navbar } from "@/components/Navbar";
+
 
 export function Home() {
   return (
     <div>
-      <Navbar />
+      
       <Hero />
       <Features />
       <ChooseUs />
       <GoDashboard />
-      <Footer />
+      
     </div>
   );
 }
