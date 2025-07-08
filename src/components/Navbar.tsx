61f07614f8682e8add99c5c23fec3db53eecb1e2
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-07-08 09:58:57 -0300

    :recycle: Refatoração da Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router";
+import { Button } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 
 export function Navbar() {
   return (
@@ -16,25 +18,11 @@ export function Navbar() {
           <span className="text-xl font-bold text-foreground">ConnectUS</span>
         </div>
 
-        <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 text-primary-foreground h-10 px-4 py-2 bg-primary hover:bg-primary/90">
+        <Button>
           <Link to="/dashboard">Acessar Dashboard</Link>
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="ml-2 h-4 w-4"
-          >
-            <path d="M5 12h14" />
-            <path d="m12 5 7 7-7 7" />
-          </svg>
-        </button>
+          <ArrowRight size={24} />
+        </Button>
       </div>
     </header>
   );
