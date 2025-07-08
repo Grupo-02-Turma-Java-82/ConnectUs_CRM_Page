faeae90457c133f74ed0058d06c70e5e8c2e27b3
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-07-08 10:34:27 -0300

    :recycle: Refatoração da Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,10 @@
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
+import { Button } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 
 export function Hero() {
+  const navigate = useNavigate();
+
   return (
     <section className="py-20 px-6">
       <div className="container mx-auto text-center max-w-4xl">
@@ -16,27 +20,22 @@ export function Hero() {
           poderosa.
         </p>
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 text-primary-foreground h-11 rounded-md bg-primary hover:bg-primary/90 text-lg px-8 py-6">
-            <Link to="/dashboard">Começar Agora</Link>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-arrow-right ml-2 h-5 w-5"
-            >
-              <path d="M5 12h14"></path>
-              <path d="m12 5 7 7-7 7"></path>
-            </svg>
-          </button>
-          <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border bg-background hover:text-accent-foreground h-11 rounded-md text-lg px-8 py-6 border-primary text-primary hover:bg-primary/10">
+          <Button
+            className="h-11 text-lg py-6 cursor-pointer"
+            size="lg"
+            onClick={() => navigate("/dashboard")}
+          >
+            Começar Agora
+            <ArrowRight size={24} />
+          </Button>
+
+          <Button
+            className="h-11 text-lg py-6 cursor-pointer test-primary"
+            variant="outline"
+            onClick={() => navigate("/customers")}
+          >
             Ver Demonstração
-          </button>
+          </Button>
         </div>
       </div>
     </section>
